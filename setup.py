1a647b7399f28a39ed2255aaad5a67623e6ca150
Rashmi Janve <rashmijanve0801@gmail.com>
2026-03-23 18:42:47 +0530

    Setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="realtime-source-code-analyzer",
+    version="0.1.0",
+    author="Rashmi",
+    author_email="rashmijanve0801@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
